f2a439bcda6ed7fa8e7054ee0264d8eb9bcc4e74
jaidanbrown <jaidanbrown1999@gmail.com>
2022-01-30 18:33:25 +0000

    Added button styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './sass/main.scss'
 
 function App() {
-	return <div className='app'></div>
+	return (
+		<div className='app'>
+			<button class='button' data-number='01'>
+				Button
+			</button>
+		</div>
+	)
 }
 
 export default App
